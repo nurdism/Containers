04e46fd708ac35adf6e124be71b4f28848e3f4fb
Digitroinc <craigbett@gmail.com>
2017-07-26 12:15:51 -0400

    //WIP

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sleep 5
+
+#Install the Server
+if [[ ! -d /home/container/server ]] || [[ ${UPDATE} == "1" ]]; then
+	if [[ -f /home/container/steam.txt ]]; then
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +runscript /home/container/steam.txt
+	else
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +quit
+	fi
+fi
+
+if [[ -f /home/container/preflight.sh ]]; then
+	/home/container/preflight.sh
+fi
+
+cd /home/container/server
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container/server$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
+
+if [ $? -ne 0 ]; then
+    echo "PTDL_CONTAINER_ERR: There was an error while attempting to run the start command."
+    exit 1
+fi
