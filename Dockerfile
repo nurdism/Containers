1adbf970aff74efa67bd83a266a7607a1237050f
Digitroinc <craigbett@gmail.com>
2017-08-08 12:43:25 -0400

    //wip

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# ----------------------------------
+# Pterodactyl Core Dockerfile
+# Environment: Garrys Mod
+# Minimum Panel Version: 0.6.0
+# ----------------------------------
+FROM        ubuntu:14.04
+
+MAINTAINER  Nerdism, <nerd@nerdism.net>
+ENV         DEBIAN_FRONTEND noninteractive
+
+RUN         dpkg --add-architecture i386 \
+            && apt-get update \
+            && apt-get upgrade -y \
+            && apt-get install -y tar bzip2 curl gcc g++ lib32gcc1 lib32tinfo5 lib32z1 lib32stdc++6 libtinfo5:i386 libncurses5:i386 libcurl3-gnutls:i386 \
+            && curl -sL https://deb.nodesource.com/setup_6.x | bash - \
+            && apt install -y nodejs \
+            && useradd -m -d /home/container container
+
+USER        container
+ENV         HOME /home/container
+WORKDIR     /home/container
+
+COPY        ./entrypoint.sh /entrypoint.sh
+COPY        ./wrapper.js /wrapper.js
+
+CMD         ["/bin/bash", "/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+sleep 5
+
+#Install the Server
+if [[ ${UPDATE} == "1" ]]; then
+	if [[ -f /home/container/steam.txt ]]; then
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${APP_ID} validate +runscript /home/container/steam.txt
+	else
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${APP_ID} validate +quit
+	fi
+fi
+
+if [[ ! -L /home/container/.steam/sdk32/steamclient.so ]]; then
+	mkdir -p /home/container/.steam/sdk32
+	cd /home/container/.steam/sdk32
+	ln -s ../../steamcmd/linux32/steamclient.so steamclient.so
+fi
+
+if [[ -f /home/container/preflight.sh ]]; then
+	/home/container/preflight.sh
+fi
+
+cd /home/container
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+#Check port allocation
+if [ -z "${ALLOC_0__PORT}" ] || [ "${ALLOC_0__PORT}" != "${FDL_PORT}" ]; then
+    echo "---=== Please add fastdl port to the server as an additional allocation, or you will be unable to use fastdl. ===---"
+    sleep 10
+    exit 1
+fi
+
+node ./wrapper.js ${FDL_PORT} ${MODIFIED_STARTUP}
+
+if [ $? -ne 0 ]; then
+    echo "PTDL_CONTAINER_ERR: There was an error while attempting to run the start command."
+    exit 1
+fi
