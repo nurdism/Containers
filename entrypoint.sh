7ba9148e8603df080c78013a42803e256dca36fe
Digitroinc <craigbett@gmail.com>
2017-08-08 14:50:37 -0400

    //wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,117 @@
 #!/bin/bash
 sleep 5
 
+fastdl_search() {
+
+	SOUND="$1/sound"
+	RESOURCE="$1/resource"
+	PARTICLES="$1/particles"
+	MODELS="$1/models"
+	MATERIALS="$1/materials"
+	MAPS="$1/maps"
+
+	if [ ! -d "$2" ]; then
+		mkdir "$2"
+	fi
+
+	if [[ -d "${SOUND}" ]]; then
+		if [ ! -d "$2/sound" ]; then
+			mkdir "$2/sound"
+		fi
+		fastdl_add "${SOUND}" "$2/sound"
+	fi
+
+	if [[ -d "${RESOURCE}" ]]; then
+		if [ ! -d "$2/resource" ]; then
+			mkdir "$2/resource"
+		fi
+		fastdl_add "${RESOURCE}" "$2/resource"
+	fi
+
+	if [[ -d "${PARTICLES}" ]]; then
+		if [ ! -d "$2/particles" ]; then
+			mkdir "$2/particles"
+		fi
+		fastdl_add "${PARTICLES}" "$2/particles"
+	fi
+
+	if [[ -d "${MODELS}" ]]; then
+		if [ ! -d "$2/models" ]; then
+			mkdir "$2/models"
+		fi
+		fastdl_add "${MODELS}" "$2/models"
+	fi
+
+	if [[ -d "$MATERIALS" ]]; then
+		if [ ! -d "$2/materials" ]; then
+			mkdir "$2/materials"
+		fi
+		fastdl_add "$MATERIALS" "$2/materials"
+	fi
+
+	if [[ -d "$MAPS" ]]; then
+		if [ ! -d "$2/maps" ]; then
+			mkdir "$2/maps"
+		fi
+		fastdl_add "$MAPS" "$2/maps"
+	fi
+
+}
+
+fastdl_add() {
+
+    echo "Checking for content in: ${1}"
+
+	for FOLDER in $1/*; do
+		find "${FOLDER}" -type f | while read FILE; do
+			MOVEF=$(echo $FILE | sed "s|$1|$2|g")
+			MOVED=$(dirname "${MOVEF}")
+			if [ ! -d "${MOVED}" ]; then
+				mkdir -p "${MOVED}"
+			fi
+			BZ2="${MOVEF}.bz2"
+			if [[ ! -f $BZ2 ]]; then
+				echo "Adding: ${FILE} -> ${MOVEF}"
+				cp "${FILE}" "${MOVEF}"
+			fi
+		done
+    done
+}
+
+# Adding Fastdl Support
+
+if [[ ${FASTDL} == "1" ]]; then
+    MAPS="/home/container/garrysmod/maps"
+    ADDONS="/home/container/garrysmod/addons"
+    GAMEMODES="/home/container/garrysmod/gamemodes"
+    FASTDL="/home/container/fastdl"
+
+    echo "Setting up Fast DL content! ( Can take a few minutes to complete! )"
+
+    fastdl_add "$MAPS" "$FASTDL/maps"
+
+    find "${ADDONS}" -maxdepth 1 -type d| while read FOLDER; do
+        if [[ $ADDONS != $FOLDER ]]; then
+            fastdl_search "$FOLDER" "$FASTDL"
+        fi
+    done
+
+    find "${GAMEMODES}" -maxdepth 1 -type d| while read FOLDER; do
+        if [[ $GAMEMODES != $FOLDER ]]; then
+            fastdl_search "$FOLDER/content" "$FASTDL"
+        fi
+    done
+
+    find "${FASTDL}" -type f \( ! -iname "*.bz2" \) | while read FILE_NAME; do
+        BZ2="${FILE_NAME}.bz2"
+        if [[ ! -f "${BZ2}" ]]; then
+            echo "Archiving: ${FILE_NAME}"
+            bzip2 "${FILE_NAME}"
+        fi
+    done
+fi
+
+
 #Install the Server
 if [[ ! -f /home/container/srcds_run ]] || [[ ${UPDATE} == "1" ]]; then
 	if [[ -f /home/container/steam.txt ]]; then
@@ -10,14 +121,11 @@ if [[ ! -f /home/container/srcds_run ]] || [[ ${UPDATE} == "1" ]]; then
 	fi
 fi
 
+#steamclient.so fix
 if [[ ! -L /home/container/.steam/sdk32/steamclient.so ]]; then
 	mkdir -p /home/container/.steam/sdk32
 	cd /home/container/.steam/sdk32
 	ln -s ../../steamcmd/linux32/steamclient.so steamclient.so
-fi
-
-if [[ -f /home/container/preflight.sh ]]; then
-	/home/container/preflight.sh
 fi
 
 cd /home/container
@@ -27,13 +135,13 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 #Check port allocation
-if [ -z "${ALLOC_0__PORT}" ] || [ "${ALLOC_0__PORT}" != "${FDL_PORT}" ]; then
+if  [ ${FASTDL} != "1" ] && [ -z "${ALLOC_0__PORT}" ] && [ "${ALLOC_0__PORT}" != "${FASTDL_PORT}" ]; then
     echo "---=== Please add fastdl port to the server as an additional allocation, or you will be unable to use fastdl. ===---"
     sleep 10
     exit 1
 fi
 
-node ./wrapper.js ${FDL_PORT} ${MODIFIED_STARTUP}
+node /wrapper.js ${FASTDL_PORT} ${MODIFIED_STARTUP}
 
 if [ $? -ne 0 ]; then
     echo "PTDL_CONTAINER_ERR: There was an error while attempting to run the start command."
--- wrapper.js
+++ wrapper.js
@@ -3,34 +3,36 @@ const path = require('path');
 const cwd = process.cwd();
 const fs = require('fs');
 
-const port = process.argv[2];
-require('http')
-  .createServer((req, res) => {
-    fs.readFile(path.join(cwd, 'fastdl', req['url']), (err, data) => {
-      if (err) {
-        res.writeHead(404);
-        res.end("<!DOCTYPE html><html><body><h1>File not found!</h1></body></html>");
-        return;
-      }
-      res.writeHead(200);
-      res.end(data);
-    });
-}).listen(port);
+const port = parseInt(process.argv[2]);
 
-const gmod = spawn( process.argv[3], process.argv.slice(3, process.argv.length));
-gmod.stdin.setEncoding('utf-8');
-gmod.stdout.pipe(process.stdout);
+if(port){
+  require('http')
+    .createServer((req, res) => {
+      fs.readFile(path.join(cwd, 'fastdl', req['url']), (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.end("<!DOCTYPE html><html><body><h1>File not found!</h1></body></html>");
+          return;
+        }
+        res.writeHead(200);
+        res.end(data);
+      });
+    }).listen(port, () => { console.log(`Webserver listening on port: ${port}`) });
+}
 
-process.stdin.setEncoding("utf8");
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    gmod.stdin.write(chunk);
+let exe = process.argv[3];
+let args = process.argv.slice(4, process.argv.length).map( (arg,i,a) => {
+  if (( arg.startsWith("-") || arg.startsWith("+"))) {
+    if( a[i+1].startsWith("-") || a[i+1].startsWith("+")){
+      return arg
+    }else{
+      return `${arg} ${a[i+1]}`
+    }
   }
-});
+}).filter((n) => n != undefined);
 
+const gmod = spawn(exe, args, { cwd, shell: true, stdio: 'inherit' });
 gmod.on('exit', (code) => {
   process.exit(code);
 });
-
 
