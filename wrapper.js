1c540496ce3f2026e5a4ba573ddf0b38f8e374d7
Digitroinc <craigbett@gmail.com>
2017-08-10 21:08:44 -0400

    //wip

--- wrapper.js
+++ wrapper.js
@@ -10,47 +10,36 @@ if ( port && port > 0) {
     .createServer((req, res) => {
 
       const file = path.join(cwd, 'fastdl', req['url']);
+      const ext = /(?:\.([^.]+))?$/.exec(req['url'])[1];
 
       if(!fs.existsSync(file)){
         res.writeHead(404, {'Content-type':'Content-Type: text/html; charset=utf-8'});
         res.write('<!DOCTYPE html><html><body><h1>File not found!</h1></body></html>');
         res.end();
-      }
-
-      if (req.headers['range']) {
-        const stat = fs.statSync(file);
-        const range = req.headers.range;
-        const total = stat.size;
-        const parts = range.replace(/bytes=/, "").split("-");
-
-        const start = parseInt(parts[0], 10);
-        const end = parts[1] ? parseInt(parts[1], 10) : total-1;
-
-        res.writeHead(206, {
-          'content-range': 'bytes ' + start + '-' + end + '/' + total,
-          'accept-ranges': 'bytes',
-          'content-length': (end-start) +1,
-          'content-type': ''
-        });
-
-        fs.createReadStream(file, {start: start, end: end}).pipe(res);
       }else{
-        fs.readFile(file, (err, data) => {
-          if (err) {
-            res.writeHead(404, {'Content-type':'Content-Type: text/html; charset=utf-8'});
-            res.write('<!DOCTYPE html><html><body><h1>File not found!</h1></body></html>');
-            res.end();
-          } else {
-            const ext = /(?:\.([^.]+))?$/.exec(req['url'])[1];
-            let content = 'Content-Type: application/x-bzip2';
-            if(ext !== 'bz2'){
-              content = 'Content-Type: text/html; charset=utf-8';
-            }
-            res.writeHead(200, {'Content-type': content});
+        if(ext !== 'bz2'){
+          fs.readFile(file, (err, data) => {
+            res.writeHead(200, {'Content-type': 'Content-Type: text/html; charset=utf-8'});
             res.write(data);
             res.end();
-          }
-        });
+          });
+        }else{
+          const stat = fs.statSync(file);
+          const range = req.headers.range;
+          const total = stat.size;
+          const parts = range.replace(/bytes=/, "").split("-");
+          const start = parseInt(parts[0], 10);
+          const end = parts[1] ? parseInt(parts[1], 10) : total-1;
+
+          res.writeHead(206, {
+            'content-range': 'bytes ' + start + '-' + end + '/' + total,
+            'accept-ranges': 'bytes',
+            'content-length': (end-start) +1,
+            'content-type': 'application/octet-stream'
+          });
+
+          fs.createReadStream(file, {start: start, end: end}).pipe(res);
+        }
       }
     }).listen(port, () => { console.log(`Webserver listening on port: ${port}`) });
 }
