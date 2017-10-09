ab481c55113df1cee9ded3904753cbc81febc14c
Digitroinc <craigbett@gmail.com>
2017-10-09 07:22:03 -0400

    //wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,15 @@ sleep 5
 
 cd /home/container
 
+#Install the Server
+if [[ ! -f /home/container/srcds_run ]] || [[ ${UPDATE} == "1" ]]; then
+	if [[ -f /home/container/steam.txt ]]; then
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${APP_ID} validate +runscript /home/container/steam.txt
+	else
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${APP_ID} validate +quit
+	fi
+fi
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
