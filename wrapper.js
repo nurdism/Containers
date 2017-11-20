55636c90c5f3da8d83b5d2e1f5bc33eef8176d8b
Digitroinc <craigbett@gmail.com>
2017-11-19 23:29:55 -0500

    //wip

--- wrapper.js
+++ wrapper.js
@@ -174,46 +174,6 @@ const Tail = (function(superClass) {
 
 })(events.EventEmitter);
 
-const stamp = (pattern, date) => {
-  if (typeof pattern !== 'string') {
-    date = pattern;
-    pattern = 'YYYY-MM-DD';
-  }
-
-  if (!date) date = new Date();
-
-  function timestamp() {
-    const regex = /(?=(YYYY|YY|MM|DD|HH|mm|ss|ms))\1([:\/]*)/;
-    const match = regex.exec(pattern);
-
-    if (match) {
-      const increment = method(match[1]);
-      const val = '00' + String(date[increment[0]]() + (increment[2] || 0));
-      const res = val.slice(-increment[1]) + (match[2] || '');
-      pattern = pattern.replace(match[0], res);
-      timestamp();
-    }
-  }
-
-  function method(key) {
-    return ({
-      YYYY: ['getFullYear', 4],
-      YY: ['getFullYear', 2],
-      // getMonth is zero-based, thus the extra increment field
-      MM: ['getMonth', 2, 1],
-      DD: ['getDate', 2],
-      HH: ['getHours', 2],
-      mm: ['getMinutes', 2],
-      ss: ['getSeconds', 2],
-      ms: ['getMilliseconds', 3]
-    })[key];
-  }
-
-  timestamp(pattern);
-  return pattern;
-};
-
-
 const exe =  path.join(cwd, '7DaysToDieServer.x86');
 fs.access( exe, fs.constants.X_OK, (err) => {
     if (err) {
@@ -230,7 +190,7 @@ fs.access( exe, fs.constants.X_OK, (err) => {
 
         const tail = new Tail(logfile);
         tail.on("line", (data) => {
-            console.log(`[${stamp("MM:DD HH:mm:ss")}] ${data}`);
+            console.log(data);
         });
 
         let args = process.argv.slice(2);
