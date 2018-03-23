c97c3c3260a0fb7de5870c2c6f3e99f3c9e83c60
Digitroinc <craigbett@gmail.com>
2018-03-22 21:43:39 -0400

    //wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ sleep 1
 
 echo "Setting up CitizenFX Server"
 
-if [[ ! -d cfx-server-data ]]; then
+cd /home/container
+
+if [[ ! -d .git ]]; then
     echo "Downloading CitizenFX server data"
-    cd /home/container
-    git clone https://github.com/citizenfx/cfx-server-data.git cfx-server-data
+    git clone https://github.com/citizenfx/cfx-server-data.git
 else
     if [[ ${UPDATE} == "1" ]]; then
         echo "Updating CitizenFX server data"
-        cd /home/container/cfx-server-data
         git fetch origin master
         git reset --hard FETCH_HEAD
     fi
@@ -19,15 +19,12 @@ fi
 
 if [[ ! -d cfx-server ]] || [[ ${UPDATE} == "1" ]]; then
     echo "Downloading CitizenFX server"
-    cd /home/container
     wget https://m-84g4dtu6fd76.runkit.sh/?q=linux -O fx.tar.xz
     tar xf fx.tar.xz alpine/opt/
     mv -f alpine/opt/cfx-server/ ./
     chmod +x ./cfx-server/FXServer
     rm -rf fx.tar.xz alpine/
 fi
-
-cd /home/container
 
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
